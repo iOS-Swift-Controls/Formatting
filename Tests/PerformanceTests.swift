f9b737032dcbdb7f2f5b8cff7d9327a9f0627d34
kean <grebenyuk.alexander@gmail.com>
2020-11-29 17:22:50 -0500

    Add playground

--- Tests/PerformanceTests.swift
+++ Tests/PerformanceTests.swift
@@ -1,127 +1,32 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2019 Alexander Grebenyuk (github.com/kean).
+// Copyright (c) 2020 Alexander Grebenyuk (github.com/kean).
 
 import XCTest
-import Regex
+import Formatting
 
-class PerformanceNearlyMatchingInputTests: XCTestCase {
+class FormattingsPerformanceTests: XCTestCase {
+    let input = """
+    Let's test <b>bold</b>.<br><br>And a link <a href="http://google.com">google</a>.
+    """
 
-    // NSRegularExpression: 0.336 seconds
-    // Regex: 0.031 seconds
-    func testNearlyMatchingPatternWithGreedyQuantifier() throws {
-        let regex = try Regex("a*c")
-        let string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
+    func testFormattingPerformance() {
+        let style = FormattedStringStyle(attributes: [
+            "body": [.font: UIFont(name: "HelveticaNeue-Light", size: 20)!]
+        ])
 
         measure {
-            for _ in 0...2000 {
-                _ = regex.matches(in: string)
+            for _ in 0...50 {
+                let _ = NSAttributedString(formatting: input, style: style)
             }
         }
     }
 
-    func testMultipleMatches() throws {
-        let regex = try Regex("a")
-        let string = String(repeating: "a", count: 100_000)
-
+    func testHTMLParsingPerformance() {
+        let data = input.data(using: .utf8)!
         measure {
-            _ = regex.matches(in: string)
-        }
-    }
-
-    // NSRegularExpression: didn't finish in a reasonable time
-    // Regex: 0.24 seconds
-    func testNearlyMatchingPatternWithLongInput() throws {
-        let regex = try Regex("a*c")
-        let string = String(repeating: "a", count: 75_000) + "b"
-
-        measure {
-            _ = regex.matches(in: string)
-        }
-    }
-
-    // NSRegularExpression: 0.106 seconds
-    // Regex: 0.027 seconds
-    func testNearlyMatchingPatternWithNestedGreedyQuantifier2() throws {
-        let regex = try Regex("(aa)*c")
-        let string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
-
-        measure {
-            for _ in 0...1500 {
-                _ = regex.matches(in: string)
-            }
-        }
-    }
-
-    // NSRegularExpression: didn't finish in a reasonable time
-    // Regex: 0.028 seconds
-    func testNearlyMatchingPatternWithNestedGreedyQuantifier() throws {
-        let regex = try Regex("(a*)*c")
-        let string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
-
-        measure {
-            for _ in 0...1000 {
-                _ = regex.matches(in: string)
-            }
-        }
-    }
-
-    // NSRegularExpression: didn't finish in a reasonable time
-    // Regex: 0.026 seconds
-    func testNearlyMatchingPattern() throws {
-        let regex = try Regex("X(.+)+X")
-        let string = "=XX========================================="
-
-        measure {
-             for _ in 0...1000 {
-                 _ = regex.matches(in: string)
-             }
-         }
-    }
-
-    // NSRegularExpression: 0.004 seconds
-    // Regex: 0.027 seconds
-    func testNearlyMatchingSubstring() throws {
-        let p = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac"
-        let s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
-
-        let regex = try Regex(p)
-
-        measure {
-            for _ in 0...1000 {
-                _ = regex.matches(in: s)
-            }
-        }
-    }
-
-    // NSRegularExpression: 0.036 seconds
-    // Regex: 0.086 seconds
-    func testNearlyMatchingSubstringCreateWithRange() throws {
-        let regex = try Regex("a{1000}c")
-        let string = String(repeating: "a", count: 5_000) + "b"
-
-        measure {
-            _ = regex.matches(in: string)
-        }
-    }
-
-    // NSRegularExpression: 0.036 seconds
-    // Regex: 0.088 seconds
-    func testNearlyMatchingSubstringCreateWithRangeAndAlternation() throws {
-        let regex = try Regex("(a{1000}|a{800})c")
-        let string = String(repeating: "a", count: 3_000) + "b"
-
-        measure {
-            _ = regex.matches(in: string)
-        }
-    }
-}
-
-class PerformanceParsingTests: XCTestCase {
-    func testParsing() {
-        measure {
-            for _ in 0...500 {
-                _ = try! Regex(#"^[-]?[0-9]+[,.]?[0-9]*([\/][0-9]+[,.]?[0-9]*)*$"#)
+            for _ in 0...50 {
+                let _ = try! NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil)
             }
         }
     }
